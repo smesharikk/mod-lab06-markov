fad0311d5b52a1f0c0ab4a1306222510dd0201c0
smesharikk <93827477+smesharikk@users.noreply.github.com>
2024-06-08 23:58:36 +0300

    Add files via upload

---
+++ include/textgen.h
@@ -0,0 +1,22 @@
+// Copyright 2024 unn
+#ifndef INCLUDE_TEXTGEN_H_
+#define INCLUDE_TEXTGEN_H_
+#include <string>
+#include <deque>
+#include <vector>
+#include <map>
+class Gen {
+ private:
+    std::string data;
+    std::vector<std::string> words;
+    typedef std::deque<std::string> prefix;
+    int NPREF;
+    int MAXGEN;
+ public:
+    std::map<prefix, std::vector<std::string> > statetab;
+    Gen(std::string arr, int val1, int val2);
+    std::string getText();
+    Gen(std::map<prefix, std::vector<std::string> > val,
+        std::vector<std::string> words, int val1, int val2);
+};
+#endif // INCLUDE_TEXTGEN_H_
