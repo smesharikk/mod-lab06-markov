67a136ae5cf0ceaac8ef139120368b04fc5b7f80
smesharikk <davsoghomonyan04@mail.ru>
2024-06-09 18:23:00 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -11,16 +11,16 @@
 #include <vector>
 
 class MarkovGenerator {
-public:
-	typedef std::deque<std::string> prefix;
-	std::map<prefix, std::vector<std::string>> statetab;
-	std::deque<std::string> start;
-	std::deque<std::string> next;
-	int NPREF;
-	int MAXGEN;
-	int countWords = 0;
+ public:
+  typedef std::deque<std::string> prefix;
+  std::map<prefix, std::vector<std::string>> statetab;
+  std::deque<std::string> start;
+  std::deque<std::string> next;
+  int NPREF;
+  int MAXGEN;
+  int countWords = 0;
 
-	MarkovGenerator(int kolvo, int size);
-	void readFromFile(std::string path);
-	void generateNewText();
+  MarkovGenerator(int kolvo, int size);
+  void readFromFile(std::string path);
+  void generateNewText();
 };
