f760b7a2bd09c3ad49bbd2708e728d4627eef1d1
smesharikk <93827477+smesharikk@users.noreply.github.com>
2024-06-09 15:11:45 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -8,11 +8,11 @@
 class Gen {
  private:
     std::string data;
-    std::vector<std::string> words;
-    typedef std::deque<std::string> prefix;
     int NPREF;
     int MAXGEN;
  public:
+    std::vector<std::string> words;
+    typedef std::deque<std::string> prefix;
     std::map<prefix, std::vector<std::string> > statetab;
     Gen(std::string arr, int val1, int val2);
     std::string getText();
