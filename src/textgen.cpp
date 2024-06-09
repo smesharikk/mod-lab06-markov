b8bb284fcef866c0b8e2e80a099493df5056a0fb
smesharikk <davsoghomonyan04@mail.ru>
2024-06-09 18:02:31 +0300

    Update textgen.cpp

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -3,46 +3,46 @@
 #include "textgen.h"
 
 MarkovGenerator::MarkovGenerator(int kolvo, int size) {
-	NPREF = kolvo;
-	MAXGEN = size;
+    NPREF = kolvo;
+    MAXGEN = size;
 }
 
 void MarkovGenerator::readFromFile(std::string path) {
-	std::ifstream in(path);
-	std::string str;
-	for (int i = 0; i < NPREF; i++) {
-		in >> str;
-		next.push_back(str);
-	}
-	start = next;
-	while (in >> str) {
-		statetab[next].push_back(str);
-		next.pop_front();
-		next.push_back(str);
-	}
-	in.close();
+    std::ifstream in(path);
+    std::string str;
+    for (int i = 0; i < NPREF; i++) {
+        in >> str;
+        next.push_back(str);
+    }
+    start = next;
+    while (in >> str) {
+        statetab[next].push_back(str);
+        next.pop_front();
+        next.push_back(str);
+    }
+    in.close();
 }
 
 void MarkovGenerator::generateNewText() {
-	std::ofstream out("output.txt");
-	for (int i = 0; i < NPREF; i++) {
-		out << start[i] << " ";
-	}
-	std::map<prefix, std::vector<std::string>>::iterator it;
-	next = start;
-	std::string suff;
-	srand(time(0));
-	for (int i = 0; i < MAXGEN; i++) {
-		it = statetab.find(next);
-		if (it != statetab.end()) {
-			suff = it->second[rand() % (it->second).size()];
-			out << suff << " ";
-			next.pop_front();
-			next.push_back(suff);
-			countWords++;
-		}
-		else {
-			break;
-		}
-	}
+    std::ofstream out("output.txt");
+    for (int i = 0; i < NPREF; i++) {
+        out << start[i] << " ";
+    }
+    std::map<prefix, std::vector<std::string>>::iterator it;
+    next = start;
+    std::string suff;
+    srand(time(0));
+    for (int i = 0; i < MAXGEN; i++) {
+        it = statetab.find(next);
+        if (it != statetab.end()) {
+            suff = it->second[rand() % (it->second).size()];
+            out << suff << " ";
+            next.pop_front();
+            next.push_back(suff);
+            countWords++;
+        }
+        else {
+            break;
+        }
+    }
 }
